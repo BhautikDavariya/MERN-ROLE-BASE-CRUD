5a38c71d1501cbfd3c81fdfecc9b5876424052a7
Bhautik Davariya <bhautikmerndevelopers@gmail.com>
2023-07-05 09:41:41 +0530

    Minor change User Action

--- clint/src/store/actions/userAction.js
+++ clint/src/store/actions/userAction.js
@@ -38,3 +38,26 @@ export const addUser = (data) => async (dispatch) => {
         toast(error.data.message, errorOptions)
     })
 }
+
+
+// export const fetchLinks = (filter = {}, isLoading = true) => async (dispatch) => {
+//     if (isLoading) {
+//         dispatch(setLoading(true))
+//     }
+//     let url = apiBaseURL.LINK;
+//     if (!_.isEmpty(filter) && (filter.page || filter.pageSize || filter.search || filter.order_By || filter.created_at)) {
+//         url += requestParam(filter);
+//     }
+//     apiConfig.get(url)
+//         .then((response) => {
+//             dispatch({ type: linkActionType.FETCH_LINKS, payload: response.data.data });
+//             dispatch(setTotalRecord(response.data.meta.total))
+//             if (isLoading) {
+//                 dispatch(setLoading(false))
+//             }
+//         })
+//         .catch((response) => {
+//             response?.response?.data?.message?.includes('Your plan has been expired') ? '' : dispatch(addToast(
+//                 { text: response.response.data.title || response.response.data.message, type: toastType.ERROR }));
+//         });
+// };
